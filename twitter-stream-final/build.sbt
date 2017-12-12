f54b16c4cd53ed9025bacd866f3b2c672717067d
Thomas Curry <thomasjcurry@hotmail.com>
2017-12-12 23:31:02 +0000

    Downgrade compose version for compatability. add the sbt proj to run against es

---
+++ twitter-stream-final/build.sbt
@@ -0,0 +1,7 @@
+name := "twitter-stream-final"
+
+version := "0.1"
+
+scalaVersion := "2.11.0"
+
+val sparkVersion = "2.2.0"
---
+++ twitter-stream-final/src/main/scala/com/qq/spark/Utilities.scala
@@ -0,0 +1,39 @@
+package com.qq.spark
+
+import org.apache.log4j.Level
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
+
+object Utilities {
+  /** Makes sure only ERROR messages get logged to avoid log spam. */
+  def setupLogging() = {
+    import org.apache.log4j.{Level, Logger}
+    val rootLogger = Logger.getRootLogger()
+    rootLogger.setLevel(Level.ERROR)
+  }
+
+  /** Configures Twitter service credentials using twiter.txt in the main workspace directory */
+  def setupTwitter() = {
+    import scala.io.Source
+
+    val stream = getClass.getResourceAsStream("/twitter.txt")
+    for (line <- Source.fromInputStream(stream).getLines) {
+      val fields = line.split(" ")
+      if (fields.length == 2) {
+        System.setProperty("twitter4j.oauth." + fields(0), fields(1))
+      }
+    }
+  }
+}
+
+object SQLContextSingleton {
+
+  @transient  private var instance: SQLContext = _
+
+  def getInstance(sparkContext: SparkContext): SQLContext = {
+    if (instance == null) {
+      instance = new SQLContext(sparkContext)
+    }
+    instance
+  }
+}
